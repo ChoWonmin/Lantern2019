0a094aa11fe5b2335a817fe3f85e9c52f92301be
Suhkyung Bae <suhkyungbae@Suhkyungui-MacBook-Pro.local>
2019-04-05 14:56:57 +0900

    Search AutoComplete

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,13 +2,24 @@
     v-content
         v-layout
         v-flex.xs12
-        v-text-field(label="Solo" placeholder="현재위치" solo)
+        //v-text-field(label="Solo" placeholder="현재위치" solo)
+        v-autocomplete.offset-xs12( :box = "true" background-color = "white" :full-width = "true" :items="states" :placeholder ="'지역을 입력하세요'")
+            template v-slot:append-outer
+                v-slide-x-reverse-transition(mode="out-in")
 </template>
 
 <script>
+
+
     export default {
         data() {
-            return {};
+            return {
+                isEditing: false,
+                model: null,
+                states: [
+                    'Seoul, Republic of Korea', 'Osaka, Japan', 'Tokyo, Japan', 'Barcelona, Spain',
+                    'Taipei, Taiwan', 'Jeju, Republic of Korea', 'London, UK'
+                ]};
         },
         methods: {
         }
