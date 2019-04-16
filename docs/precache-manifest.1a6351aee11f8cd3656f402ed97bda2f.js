e901b4e6abfc0d945241e6388ded3cf6c0d21463
ChoWonmin <0108257@gmail.com>
2019-04-16 13:38:12 +0900

    github deploy

---
+++ docs/precache-manifest.1a6351aee11f8cd3656f402ed97bda2f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Lantern2019/robots.txt"
+  },
+  {
+    "revision": "ee3ca1479227046e95c4",
+    "url": "/Lantern2019/js/chunk-vendors.3e99b4fc.js"
+  },
+  {
+    "revision": "efb57db0bc01365c0f51",
+    "url": "/Lantern2019/js/app.c70fc7e0.js"
+  },
+  {
+    "revision": "ada8a924029c529793ed",
+    "url": "/Lantern2019/js/about.f526c89a.js"
+  },
+  {
+    "revision": "4312a8f1d544b39942f9da8db7be6011",
+    "url": "/Lantern2019/index.html"
+  },
+  {
+    "revision": "1a9dca4e8da9be313b25fe7aa3d983e5",
+    "url": "/Lantern2019/img/profile01.1a9dca4e.jpg"
+  },
+  {
+    "revision": "195b0e1edb16dd48a5ac9567fce4cdfd",
+    "url": "/Lantern2019/img/logo_lantern.195b0e1e.svg"
+  },
+  {
+    "revision": "0485de75dad74c6366ccbf0b8d65edd8",
+    "url": "/Lantern2019/img/lanternlogosimple2.0485de75.svg"
+  },
+  {
+    "revision": "ee3ca1479227046e95c4",
+    "url": "/Lantern2019/css/chunk-vendors.6461ab02.css"
+  },
+  {
+    "revision": "efb57db0bc01365c0f51",
+    "url": "/Lantern2019/css/app.05c1085e.css"
+  }
+];
