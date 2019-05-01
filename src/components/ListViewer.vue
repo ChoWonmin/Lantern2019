249384a41bdc191c73430dbaf7019a25ac79d43b
ChoWonmin <0108257@gmail.com>
2019-05-01 21:03:03 +0900

    add ListViewer

---
+++ src/components/ListViewer.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  v-content
+    v-content.list-header
+      .list-header-text {{title}}
+    v-content(v-show="open").lantern-list
+      v-card(v-for="lantern in items")
+        v-layout(row wrap)
+          v-flex(align-self-center xs6)
+            v-img(:src="require('../assets/profile01.jpg')" height="180")
+          v-flex(xs6)
+            v-card-title
+              v-layout(column)
+                .headline.name {{lantern.name}}
+                div.font-weight-light.body-2
+                  .infomation {{`( ${lantern.age}, ${lantern.sex} )`}}
+                v-layout
+                  v-icon(small) language
+                  div.font-weight-light.font-italic.caption
+                    .keywords {{lantern.language.join(' ')}}
+                v-layout
+                  v-icon(small color = "yellow") star
+                  div 4.5
+                    //.rating {{lantern.rating}}
+</template>
+
+<script>
+export default {
+  props:{
+    title: {
+      type: String,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    open: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang="sass">
+
+</style>
--- src/views/LanternList.vue
+++ src/views/LanternList.vue
@@ -2,29 +2,19 @@
   v-container(align-center fill-height fluid style="overflow-y: scroll;")
     v-layout
       v-flex(xs12)
-        v-content
-          v-card(v-for="lantern in list")
-            v-layout(row wrap)
-              v-flex(align-self-center xs6)
-                v-img(:src="require('../assets/profile01.jpg')" height="180")
-              v-flex(xs6)
-                v-card-title
-                  v-layout(column)
-                    .headline.name {{lantern.name}}
-                    div.font-weight-light.body-2
-                      .infomation {{`( ${lantern.age}, ${lantern.sex} )`}}
-                    v-layout
-                      v-icon(small) language
-                      div.font-weight-light.font-italic.caption
-                        .keywords {{lantern.language.join(' ')}}
-                    v-layout
-                      v-icon(small color = "yellow") star
-                      div 4.5
-                        //.rating {{lantern.rating}}
+        ListViewer(title="나와 가장 잘 맞는 랜턴" :items="list")
+        ListViewer(title="내가 좋아요한 카드" :items="list")
+        ListViewer(title="내가 좋아요한 카드" :items="list" :open="isOpen")
 </template>
 
 <script>
+import ListViewer from '../components/ListViewer';
+
 export default {
+  components: {
+    ListViewer
+  },
+
   data() {
     return {
       list: [
@@ -43,96 +33,9 @@ export default {
           keywords: ['soccer', 'soju', 'computer'],
           sex: 'male',
           language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
-        },
-        {
-          photo: '',
-          name: 'wonmin',
-          age: 26,
-          keywords: ['soccer', 'soju', 'computer'],
-          sex: 'male',
-          language: ['Korean', 'English']
         }
-      ]
+      ],
+      isOpen: false
     }
   }
 }
