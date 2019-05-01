b1eba77beb60e8b337d14d6e9b1cfe51bcd55c70
Suhkyung Bae <suhkyungbae@Suhkyungui-MacBook-Pro.local>
2019-05-01 21:04:54 +0900

    swiper

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,16 +1,22 @@
 <template lang="pug">
-  v-layout(fill-height)
-    v-card(width="100%" height="100%").swiper
-      v-img(:src="require('../assets/profile01.jpg')" height="100%")
+    v-layout(v-touch="{left: () => swipe('Left'),right: () => swipe('Right'), up: () => swipe('Up'),down: () => swipe('Down')}",
+    column, align-center, justify-center,style="height: 500px",class="grey lighten-2") {{ swipeDirection }}
+        v-card(width="100%" height="100%").swiper
+        v-img(:src="require('../assets/profile01.jpg')" height="100%")
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {}
-};
+    export default {
+        data: () => ({
+            swipeDirection: 'None'
+        }),
+
+        methods: {
+            swipe (direction) {
+                this.swipeDirection = direction
+            }
+        }
+    };
 </script>
 
 <style scoped lang="sass">
