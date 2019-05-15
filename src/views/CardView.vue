bd74d69f492b8dd014734d8ab5ff4f4c80f68dd5
Suhkyung Bae <suhkyungbae@Suhkyungui-MacBook-Pro.local>
2019-05-15 22:39:02 +0900

    cardview update

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -2,81 +2,30 @@
     v-container(align-center fill-height fluid style="overflow-y: scroll;")
         v-layout
             v-flex(xs12)
-                v-chip(label color="pink" text-color="white") Tags
+                v-img(:src="require('../assets/profile01.jpg')" height="100%")
+                v-chip(label color= "yellow darken-3" text-color="white")
                     v-icon(left) label
+                    div.subheading.font-weight-bold Tags
+
 </template>
 
 <script>
-    import ListViewer from '../components/ListViewer';
-
+    import Header from '../components/Header';
     export default {
         components: {
-            ListViewer
+            Header
         },
 
         data() {
             return {
-                list: [
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    },
-                    {
-                        photo: '',
-                        name: 'wonmin',
-                        age: 26,
-                        keywords: ['soccer', 'soju', 'computer'],
-                        sex: 'male',
-                        language: ['Korean', 'English']
-                    }
-
-                ],
-                isOpen: false
             }
         }
     }
 </script>
+
+<style scoped lang="sass">
+    .tag-text
+
+</style>
+
+
