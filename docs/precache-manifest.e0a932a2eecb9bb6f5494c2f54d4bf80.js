f758f4f02513e9688985a0a3e27982c977b2b16b
ChoWonmin <0108257@gmail.com>
2019-05-25 16:10:56 +0900

    add logo

---
+++ docs/precache-manifest.e0a932a2eecb9bb6f5494c2f54d4bf80.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b755b3f32ae910c026589576f62f08f",
+    "url": "/Lantern2019/img/lantern_main.7b755b3f.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Lantern2019/robots.txt"
+  },
+  {
+    "revision": "e51e63ed751f134984c3",
+    "url": "/Lantern2019/js/chunk-vendors.ba11558a.js"
+  },
+  {
+    "revision": "4d2fe4582a83f7df6f4c",
+    "url": "/Lantern2019/js/app.7bf9f2a5.js"
+  },
+  {
+    "revision": "ada8a924029c529793ed",
+    "url": "/Lantern2019/js/about.f526c89a.js"
+  },
+  {
+    "revision": "3389be00b84165711c910fa88b10af72",
+    "url": "/Lantern2019/index.html"
+  },
+  {
+    "revision": "327b6e0969b8e4ac9f3ad9c4086545a0",
+    "url": "/Lantern2019/img/자산 1.327b6e09.svg"
+  },
+  {
+    "revision": "1a9dca4e8da9be313b25fe7aa3d983e5",
+    "url": "/Lantern2019/img/profile01.1a9dca4e.jpg"
+  },
+  {
+    "revision": "195b0e1edb16dd48a5ac9567fce4cdfd",
+    "url": "/Lantern2019/img/logo_lantern.195b0e1e.svg"
+  },
+  {
+    "revision": "e51e63ed751f134984c3",
+    "url": "/Lantern2019/css/chunk-vendors.353b46c0.css"
+  },
+  {
+    "revision": "4d2fe4582a83f7df6f4c",
+    "url": "/Lantern2019/css/app.c76d1cd2.css"
+  }
+];
